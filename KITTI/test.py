d996410736334fda9b5ce0e28004c6a6bb314ee9
MinkyunSeo <funboy0804@snu.ac.kr>
2024-08-25 22:43:36 +0900

    fix: update test code

--- KITTI/test.py
+++ KITTI/test.py
@@ -2,8 +2,7 @@ import sys
 
 sys.path.append('../')
 import os
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 import math
 import time
 import torch.nn as nn
@@ -13,8 +12,8 @@ from models.BUFFER import buffer
 from utils.SE3 import *
 from KITTI.dataloader import get_dataloader
 
-
 if __name__ == '__main__':
+    print("Start testing...")
     cfg = make_cfg()
     cfg[cfg.data.dataset] = cfg.copy()
     cfg.stage = 'test'
@@ -45,6 +44,7 @@ if __name__ == '__main__':
     print("Test set size:", test_loader.dataset.__len__())
     data_timer, model_timer = Timer(), Timer()
 
+    overall_time = np.zeros(10)
     with torch.no_grad():
         states = []
         num_batch = len(test_loader)
@@ -63,7 +63,7 @@ if __name__ == '__main__':
             else:
                 trans_est = np.eye(4, 4)
 
-            ####### calculate the recall of DGR #######
+            ####### calculate the recall #######
             rte_thresh = 2.0
             rre_thresh = 5.0
             trans = data_source['relt_pose'].numpy()
@@ -73,23 +73,37 @@ if __name__ == '__main__':
             states.append(np.array([rte < rte_thresh and rre < rre_thresh, rte, rre]))
 
             if rte > rte_thresh or rre > rre_thresh:
-                print(f"{i}th fragment fails, RRE：{rre}, RTE：{rte}")
-            print(f"data_time: {data_timer.diff:.4f}s "
-                  f"model_time: {model_timer.diff:.4f}s "
-                  f"ref_time: {times[0]:.4f}s "
-                  f"keypt_time: {times[1]:.4f}s "
-                  f"fps_time: {times[2]:.4f}s "
-                  f"desc_time: {times[3]:.4f}s "
-                  f"mutual_matching_time: {times[4]:.4f}s "
-                  f"inlier_time: {times[5]:.4f}s "
-                  f"correspondence_proposal_time: {times[6]:.4f}s "
-                  f"ransac_time: {times[7]:.4f}s ")
+                print(f"{i}th fragment fails, RRE：{rre:.4f}, RTE：{rte:.4f}")
+            overall_time += np.array([data_timer.diff, model_timer.diff, *times])
             torch.cuda.empty_cache()
-
+            if (i + 1) % 100 == 0 or i == num_batch - 1:
+                temp_states = np.array(states)
+                temp_recall = temp_states[:, 0].sum() / temp_states.shape[0]
+                temp_te = temp_states[temp_states[:, 0] == 1, 1].mean()
+                temp_re = temp_states[temp_states[:, 0] == 1, 2].mean()
+                print(f"[{i + 1}/{num_batch}] "
+                      f"Registration Recall: {temp_recall:.4f} "
+                      f"RTE: {temp_te:.4f} "
+                      f"RRE: {temp_re:.4f} ")
     states = np.array(states)
     Recall = states[:, 0].sum() / states.shape[0]
     TE = states[states[:, 0] == 1, 1].mean()
     RE = states[states[:, 0] == 1, 2].mean()
-    print(f'Recall of DGR: {Recall}')
-    print(f'TE of DGR: {TE}')
-    print(f'RE of DGR: {RE}')
+    print()
+    print("---------------Test Result---------------")
+    print(f'Registration Recall: {Recall:.4f}')
+    print(f'RTE: {TE:.4f}')
+    print(f'RRE: {RE:.4f}')
+    
+    average_times = overall_time / num_batch
+    print(f"Average data_time: {average_times[0]:.4f}s "
+        f"Average model_time: {average_times[1]:.4f}s ")
+    print(f"ref_time: {average_times[2]:.4f}s "
+        f"keypt_time: {average_times[3]:.4f}s "
+        f"fps_time: {average_times[4]:.4f}s "
+        f"desc_time: {average_times[5]:.4f}s "
+        f"mutual_matching_time: {average_times[6]:.4f}s "
+        f"inlier_time: {average_times[7]:.4f}s "
+        f"correspondence_proposal_time: {average_times[8]:.4f}s "
+        f"ransac_time: {average_times[9]:.4f}s ")
+
