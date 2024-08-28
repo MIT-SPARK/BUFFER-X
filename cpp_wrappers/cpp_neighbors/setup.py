e6a986afa2fcfd99f6a8c92a7ba373a7cf61f2c5
Hyungtae Lim <shapelim@mit.edu>
2024-08-28 17:05:53 -0400

    Update setup.py for supporting TBB

--- cpp_wrappers/cpp_neighbors/setup.py
+++ cpp_wrappers/cpp_neighbors/setup.py
@@ -8,19 +8,25 @@ import numpy.distutils.misc_util
 # *****************************
 
 SOURCES = ["../cpp_utils/cloud/cloud.cpp",
-             "neighbors/neighbors.cpp",
-             "wrapper.cpp"]
+           "neighbors/neighbors.cpp",
+           "wrapper.cpp"]
+
+eigen_include_dir = '/usr/include/eigen3'
+tbb_include_dir = '/usr/include'
+tbb_library_dir = '/usr/lib'
+tbb_library = 'tbb'
+kiss_matcher_include_dir = '../cpp_utils/kiss_matcher'
 
 module = Extension(name="radius_neighbors",
-                    sources=SOURCES,
-                    extra_compile_args=['-std=c++11',
-                                        '-D_GLIBCXX_USE_CXX11_ABI=0'])
-
+                   sources=SOURCES,
+                   include_dirs=[eigen_include_dir, tbb_include_dir, kiss_matcher_include_dir] + numpy.distutils.misc_util.get_numpy_include_dirs(),
+                   extra_compile_args=['-std=c++17',
+                                       '-D_GLIBCXX_USE_CXX11_ABI=0',
+                                       '-O3',
+                                       '-march=native'],
+                   extra_link_args=[f'-L{tbb_library_dir}', f'-l{tbb_library}'])
 
 setup(ext_modules=[module], include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
-
-
-
 
 
 
--- cpp_wrappers/cpp_subsampling/setup.py
+++ cpp_wrappers/cpp_subsampling/setup.py
@@ -11,13 +11,23 @@ SOURCES = ["../cpp_utils/cloud/cloud.cpp",
              "grid_subsampling/grid_subsampling.cpp",
              "wrapper.cpp"]
 
+eigen_include_dir = '/usr/include/eigen3'
+tbb_include_dir = '/usr/include'
+tbb_library_dir = '/usr/lib'
+tbb_library = 'tbb'
+
 module = Extension(name="grid_subsampling",
                     sources=SOURCES,
-                    extra_compile_args=['-std=c++11',
-                                        '-D_GLIBCXX_USE_CXX11_ABI=0'])
-
+                   include_dirs=[eigen_include_dir, tbb_include_dir] + numpy.distutils.misc_util.get_numpy_include_dirs(),
+                    extra_compile_args=['-std=c++17',
+                                        '-D_GLIBCXX_USE_CXX11_ABI=0',
+                                        '-O3',
+                                        '-march=native'],
+                   extra_link_args=[f'-L{tbb_library_dir}', f'-l{tbb_library}'])
 
 setup(ext_modules=[module], include_dirs=numpy.distutils.misc_util.get_numpy_include_dirs())
+
+
 
 
 
