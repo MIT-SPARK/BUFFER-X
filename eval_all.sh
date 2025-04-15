885a655856b33ceeae1ddad56d757fbe1d0d76c8
MinkyunSeo <funboy0804@snu.ac.kr>
2025-04-15 12:20:21 +0900

    fix: remove supersetloader, use script instead

---
+++ eval_all.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <experiment_id>"
+  exit 1
+fi
+
+EXPERIMENT_ID="$1"
+
+DATASETS=(
+  "3DMatch"
+  "3DLoMatch"
+  "Scannetpp_iphone"
+  "Scannetpp_faro"
+  "Tiers"
+  "KITTI"
+  "WOD"
+  "MIT"
+  "KAIST"
+  "ETH"
+  "Oxford"
+)
+
+for DATASET in "${DATASETS[@]}"
+do  
+  python test.py --dataset "$DATASET" --experiment_id "$EXPERIMENT_ID"
+done
