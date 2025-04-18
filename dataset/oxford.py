178f70291703a974946d1a383062978bdb005d36
MinkyunSeo <funboy0804@snu.ac.kr>
2025-04-18 14:49:50 +0900

    cleanup: remove unused function

--- dataset/oxford.py
+++ dataset/oxford.py
@@ -158,20 +158,6 @@ class OxfordDataset(Data.Dataset):
         pts = pts @ R.T + T
         return pts
 
-    @property
-    def velo2cam(self):
-        try:
-            velo2cam = self._velo2cam
-        except AttributeError:
-            R = np.array([
-                7.533745e-03, -9.999714e-01, -6.166020e-04, 1.480249e-02, 7.280733e-04,
-                -9.998902e-01, 9.998621e-01, 7.523790e-03, 1.480755e-02
-            ]).reshape(3, 3)
-            T = np.array([-4.069766e-03, -7.631618e-02, -2.717806e-01]).reshape(3, 1)
-            velo2cam = np.hstack([R, T])
-            self._velo2cam = np.vstack((velo2cam, [0, 0, 0, 1])).T
-        return self._velo2cam
-
     def get_video_odometry(self, drive, indices=None, ext='.txt', return_all=False):
         data_path = self.pc_path + '/%s/poses_kitti.txt' % drive
         if data_path not in self.oxford_cache:
