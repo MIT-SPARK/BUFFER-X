cef2fe18da1648b1f89b6a6e3c7abc68f90d508b
Hyungtae Lim <shapelim@mit.edu>
2025-07-22 14:55:28 -0400

    Put download model script and change the explanation about it

---
+++ scripts/download_pretrained_models.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+
+wget -O snapshot.zip "https://www.dropbox.com/scl/fi/jmqcrngnul2kfyw8iqsi1/snapshot.zip?rlkey=k0sy6v1s6a57p7rqzygdmnkb6&st=h4m9j1x6&dl=1"
+unzip snapshot.zip
+rm snapshot.zip
